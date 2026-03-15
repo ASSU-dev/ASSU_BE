616644075b6780327b30ae589c8f944c2bb7ed06
BAEK0111 <baekjw111@gmail.com>
2026-03-15 23:51:59 +0900

    refactor/#291: API 양식에 맞게 수정

--- src/main/java/com/assu/server/domain/chat/controller/ChatController.java
+++ src/main/java/com/assu/server/domain/chat/controller/ChatController.java
@@ -8,11 +8,11 @@ import com.assu.server.global.apiPayload.BaseResponse;
 import com.assu.server.global.apiPayload.code.status.SuccessStatus;
 import com.assu.server.global.util.PrincipalDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,17 +20,19 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@Tag(name = "Chatting", description = "채팅 관련 api")
 @RequiredArgsConstructor
 @RequestMapping("/chat")
 public class ChatController {
     private final ChatService chatService;
-    private final SimpMessagingTemplate simpMessagingTemplate;
     private final BlockService blockService;
     private final ChattingRedisPublisher chattingRedisPublisher;
 
     @Operation(
             summary = "채팅방을 생성하는 API",
-            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed80c38871ec77deced713) 채팅방을 생성합니다.\n"+
+            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed80c38871ec77deced713)\n" +
+                    "- 채팅방을 생성합니다.\n"+
+                    "\n**Request:**\n" +
                     "- adminId: Request Body, Long\n" +
                     "- partnerId: Request Body, Long\n"
     )
@@ -44,7 +46,8 @@ public class ChatController {
 
     @Operation(
             summary = "채팅방 목록을 조회하는 API",
-            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/API-1d71197c19ed819f8f70fb437e9ce62b?p=2241197c19ed816993c3c5ae17d6f099&pm=s) 채팅방 목록을 조회합니다.\n"
+            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/API-1d71197c19ed819f8f70fb437e9ce62b?p=2241197c19ed816993c3c5ae17d6f099&pm=s)\n" +
+                    "-  채팅방 목록을 조회합니다.\n"
     )
     @GetMapping("/rooms")
     public BaseResponse<List<ChatRoomListResultDTO>> getChatRoomList(
@@ -56,7 +59,9 @@ public class ChatController {
 
     @Operation(
             summary = "채팅 API",
-            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2371197c19ed80968342e2bc8fe88cee&pm=s) 메시지를 전송합니다.\n"+
+            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2371197c19ed80968342e2bc8fe88cee&pm=s)\n" +
+                    "- 메시지를 전송합니다.\n"+
+                    "\n**Request:**\n" +
                     "- roomId: Request Body, Long\n" +
                     "- senderId: Request Body, Long\n"+
                     "- receiverId: Request Body, Long\n" +
@@ -77,7 +82,9 @@ public class ChatController {
 
     @Operation(
             summary = "메시지 읽음 처리 API",
-            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2241197c19ed81ffa771cb18ab157b54&pm=s) 메시지를 읽음처리합니다.\n"+
+            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2241197c19ed81ffa771cb18ab157b54&pm=s)\n" +
+                    "- 메시지를 읽음처리합니다.\n"+
+                    "\n**PathVariable:**\n" +
                     "- roomId: Path Variable, Long\n"
     )
     @PatchMapping("rooms/{roomId}/read")
@@ -92,7 +99,9 @@ public class ChatController {
 
     @Operation(
             summary = "채팅방 상세 조회 API",
-            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2241197c19ed81399395fd66f73730af&pm=s) 채팅방을 클릭했을 때 메시지를 조회합니다.\n"+
+            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2241197c19ed81399395fd66f73730af&pm=s)\n" +
+                    "- 채팅방을 클릭했을 때 메시지를 조회합니다.\n"+
+                    "\n**PathVariable:**\n" +
                     "- roomId: Path Variable, Long\n"
     )
     @GetMapping("rooms/{roomId}/messages")
@@ -108,7 +117,9 @@ public class ChatController {
     @Operation(
             summary = "채팅방을 나가는 API" +
                     "참여자가 2명이면 채팅방이 살아있지만, 이미 한 명이 나갔다면 채팅방이 삭제됩니다.",
-            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2371197c19ed8079a6e1c2331cb4f534&pm=s) 채팅방을 나갑니다.\n"+
+            description = "# [v1.0 (2025-08-05)](https://clumsy-seeder-416.notion.site/2241197c19ed800eab45c35073761c97?v=2241197c19ed8134b64f000cc26c5d31&p=2371197c19ed8079a6e1c2331cb4f534&pm=s)\n" +
+                    "- 채팅방을 나갑니다.\n"+
+                    "\n**PathVariable:**\n" +
                     "- roomId: Path Variable, Long\n"
     )
     @DeleteMapping("rooms/{roomId}/leave")
@@ -123,7 +134,9 @@ public class ChatController {
     @Operation(
             summary = "상대방을 차단하는 API" +
                     "상대방을 차단합니다. 메시지를 주고받을 수 없습니다.",
-            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed804ba3dbf57ba36860c4) 상대방을 차단합니다.\n"+
+            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed804ba3dbf57ba36860c4)\n" +
+                    "- 상대방을 차단합니다.\n"+
+                    "\n**Request:**\n" +
                     "- opponentId: Request Body, Long\n"
     )
     @PostMapping("/block")
@@ -138,7 +151,9 @@ public class ChatController {
     @Operation(
             summary = "상대방을 차단했는지 확인하는 API" +
                     "상대방을 차단했는지 여부를 알려줍니다.",
-            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed80769521eab9660ac53f) 상대방을 차단했는지 검사합니다.\n"+
+            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed80769521eab9660ac53f)\n" +
+                    "- 상대방을 차단했는지 검사합니다.\n"+
+                    "\n**Request:**\n" +
                     "- opponentId: Request Body, Long\n"
     )
     @GetMapping("/check/block/{opponentId}")
@@ -153,7 +168,9 @@ public class ChatController {
     @Operation(
             summary = "상대방을 차단 해제하는 API" +
                     "상대방을 차단해제합니다. 앞으로 다시 메시지를 주고받을 수 있습니다.",
-            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed80b6a93fcbe277fc934c?pvs=74) 상대방을 차단 해제합니다.\n"+
+            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed80b6a93fcbe277fc934c?pvs=74)\n" +
+                    "- 상대방을 차단 해제합니다.\n"+
+                    "\n**Request:**\n" +
                     "- opponentId: Request Body, Long\n"
     )
     @DeleteMapping("/unblock")
@@ -168,7 +185,8 @@ public class ChatController {
     @Operation(
             summary = "차단한 대상을 조회합니다." +
                     "본인이 차단한 대상을 모두 조회합니다.",
-            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed8000b047d9857bcbbb2f) 차단한 대상을 조회합니다..\n"
+            description = "# [v1.0 (2025-09-25)](https://clumsy-seeder-416.notion.site/2db1197c19ed8000b047d9857bcbbb2f)\n" +
+                    "- 차단한 대상을 조회합니다..\n"
     )
     @GetMapping("/blockList")
     public BaseResponse<List<BlockResponseDTO.BlockMemberDTO>> getBlockList(
