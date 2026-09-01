ff65f8c32c1fd8af0690dcdb5c4f08776533b5ab
kimyw1018 <poppppp00@gmail.com>
2026-09-01 13:50:08 +0900

    [FEAT/#441] 컴파일 에러 수정

--- src/main/java/com/assu/server/domain/student/service/StudentServiceImpl.java
+++ src/main/java/com/assu/server/domain/student/service/StudentServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.assu.server.domain.admin.entity.Admin;
